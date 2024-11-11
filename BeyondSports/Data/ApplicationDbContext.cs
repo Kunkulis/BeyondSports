0c1a711634673729b2bf59fc8bd2a33c83f77e27
Kunkulis <kunkulis@inbox.lv>
2024-11-11 06:54:38 +0200

    Adding real players

--- BeyondSports/Data/ApplicationDbContext.cs
+++ BeyondSports/Data/ApplicationDbContext.cs
@@ -23,20 +23,17 @@ namespace BeyondSports.Data
             );
 
             modelBuilder.Entity<Player>().HasData(
-                new Player { Id = 1, Name = "Player1", Number = 1, Position = Position.Goalkeeper, Foot = Foot.Right, BirthDate = new DateOnly(1993, 1, 1), Height = 185, IsInjured = false, TeamId = 1 },
-                new Player { Id = 2, Name = "Player2", Number = 2, Position = Position.Defender, Foot = Foot.Left, BirthDate = new DateOnly(1995, 2, 1), Height = 180, IsInjured = false, TeamId = 2 },
-                new Player { Id = 3, Name = "Player3", Number = 3, Position = Position.Defender, Foot = Foot.Right, BirthDate = new DateOnly(1998, 3, 1), Height = 182, IsInjured = false, TeamId = 1 },
-                new Player { Id = 4, Name = "Player4", Number = 4, Position = Position.Midfielder, Foot = Foot.Left, BirthDate = new DateOnly(1996, 4, 1), Height = 178, IsInjured = false, TeamId = 2 },
-                new Player { Id = 5, Name = "Player5", Number = 5, Position = Position.Midfielder, Foot = Foot.Right, BirthDate = new DateOnly(1999, 5, 1), Height = 176, IsInjured = false, TeamId = 1 },
-                new Player { Id = 6, Name = "Player6", Number = 6, Position = Position.Forward, Foot = Foot.Left, BirthDate = new DateOnly(1994, 6, 1), Height = 183, IsInjured = false, TeamId = 2 },
-                new Player { Id = 7, Name = "Player7", Number = 7, Position = Position.Forward, Foot = Foot.Right, BirthDate = new DateOnly(1997, 7, 1), Height = 180, IsInjured = false, TeamId = 1 },
-                new Player { Id = 8, Name = "Player8", Number = 8, Position = Position.Goalkeeper, Foot = Foot.Left, BirthDate = new DateOnly(1992, 8, 1), Height = 190, IsInjured = false, TeamId = 2 },
-                new Player { Id = 9, Name = "Player9", Number = 9, Position = Position.Defender, Foot = Foot.Right, BirthDate = new DateOnly(2001, 9, 1), Height = 175, IsInjured = false, TeamId = 1 },
-                new Player { Id = 10, Name = "Player10", Number = 10, Position = Position.Midfielder, Foot = Foot.Left, BirthDate = new DateOnly(2000, 10, 1), Height = 177, IsInjured = false, TeamId = 2 },
-                new Player { Id = 11, Name = "Player11", Number = 11, Position = Position.Forward, Foot = Foot.Right, BirthDate = new DateOnly(1995, 11, 1), Height = 181, IsInjured = false, TeamId = 1 },
-                new Player { Id = 12, Name = "Player12", Number = 12, Position = Position.Goalkeeper, Foot = Foot.Right, BirthDate = new DateOnly(1993, 12, 1), Height = 185, IsInjured = false, TeamId = 2 },
-                new Player { Id = 13, Name = "Player13", Number = 13, Position = Position.Defender, Foot = Foot.Left, BirthDate = new DateOnly(1996, 1, 1), Height = 180, IsInjured = false, TeamId = 1 },
-                new Player { Id = 14, Name = "Player14", Number = 14, Position = Position.Midfielder, Foot = Foot.Right, BirthDate = new DateOnly(1998, 2, 1), Height = 182, IsInjured = false, TeamId = 2 }
+                //Twente Enschede FC
+                new Player { Id = 1, Name = "Mees Hilgers", Number = 2, Position = Position.Defender, Foot = Foot.Right, BirthDate = new DateOnly(2001, 5, 13), Height = 185, IsInjured = false, TeamId = 1 },
+                new Player { Id = 2, Name = "Sem Steijn", Number = 14, Position = Position.Midfielder, Foot = Foot.Right, BirthDate = new DateOnly(2001, 11, 12), Height = 173, IsInjured = false, TeamId = 1 },
+                new Player { Id = 3, Name = "Lars Unnerstall", Number = 1, Position = Position.Goalkeeper, Foot = Foot.Right, BirthDate = new DateOnly(1990, 7, 20), Height = 198, IsInjured = false, TeamId = 1 },
+                new Player { Id = 4, Name = "Sam Lammers", Number = 10, Position = Position.Forward, Foot = Foot.Both, BirthDate = new DateOnly(1997, 8, 30), Height = 191, IsInjured = false, TeamId = 1 },
+
+                //Ajax Amsterdam
+                new Player { Id = 5, Name = "Brian Brobbey", Number = 9, Position = Position.Forward, Foot = Foot.Right, BirthDate = new DateOnly(2002, 2, 2), Height = 182, IsInjured = false, TeamId = 2 },
+                new Player { Id = 6, Name = "Jorrel Hato", Number = 4, Position = Position.Defender, Foot = Foot.Left, BirthDate = new DateOnly(2006, 3, 7), Height = 182, IsInjured = false, TeamId = 2 },
+                new Player { Id = 7, Name = "Kenneth Taylor", Number = 8, Position = Position.Midfielder, Foot = Foot.Left, BirthDate = new DateOnly(2002, 5, 16), Height = 182, IsInjured = false, TeamId = 2 },
+                new Player { Id = 8, Name = "Diant Ramaj", Number = 40, Position = Position.Goalkeeper, Foot = Foot.Both, BirthDate = new DateOnly(2001, 9, 19), Height = 189, IsInjured = false, TeamId = 2 }
             );
         }
     }
--- BeyondSports/Enum/Foot.cs
+++ BeyondSports/Enum/Foot.cs
@@ -3,6 +3,7 @@
     public enum Foot
     {
         Left,
-        Right        
+        Right,
+        Both
     }
 }
